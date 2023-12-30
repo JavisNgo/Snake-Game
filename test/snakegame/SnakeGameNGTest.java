cff806c0df7e71c1ea8752681c8bafe2f966e224
JavisNgo <ngotriduc0411@gmail.com>
2023-12-30 11:23:55 +0700

    first upload

---
+++ test/snakegame/SnakeGameNGTest.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/EmptyTestNGTest.java to edit this template
+ */
+package snakegame;
+
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Admin
+ */
+public class SnakeGameNGTest {
+    
+    public SnakeGameNGTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        fail("The test case is a prototype.");
+    }
+    
+}
---
+++ test/snakegame/SnakeGameTest.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package snakegame;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Admin
+ */
+public class SnakeGameTest {
+    
+    public SnakeGameTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        fail("The test case is a prototype.");
+    }
+    
+}
